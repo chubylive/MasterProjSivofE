1ca9ff1a1640be70e484a119d27ddc2e97c4d4f0
khoa-le-nguyen <khoa.le.nguyen@gmail.com>
2022-02-19 10:29:42 -0800

    new addition

---
+++ pl/pl_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+//Date        : Sat Feb 19 00:39:03 2022
+//Host        : dv7t-7000 running 64-bit major release  (build 9200)
+//Command     : generate_target pl_wrapper.bd
+//Design      : pl_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module pl_wrapper
+   ();
+
+
+  pl pl_i
+       ();
+endmodule
